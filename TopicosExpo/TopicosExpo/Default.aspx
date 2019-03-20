31221737157cdf36b91674c5562521659989efe8
AlonsoMesen14 <totomesen@hotmail.com>
2019-03-19 18:51:25 -0600

    :)

---
+++ TopicosExpo/TopicosExpo/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TopicosExpo._Default" %>
+
+<%@ Register Assembly="GdPicture.NET.14.WEB.DocuVieware" Namespace="GdPicture14.WEB" TagPrefix="cc1" %>
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div >
+       <cc1:DocuVieware ID="DocuVieware1" runat="server" Height="1920px" Width="1080px" />
+    </div>
+       
+  
+</asp:Content>
+
+    
---
+++ TopicosExpo/TopicosExpo/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using GdPicture14.WEB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace TopicosExpo
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DocuViewareManager.SetupConfiguration(true, DocuViewareSessionStateMode.InProc, HttpRuntime.AppDomainAppPath + "\\Cache");
+            DocuViewareLicensing.RegisterKEY("0435375251735586356441524");
+        }
+    }
+}
